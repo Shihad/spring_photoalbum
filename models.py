01d865f94ae8f67af395857857e552b0448f5fae
c0071 <balakin.arseny@ya.ru>
2023-04-11 18:18:30 +0500

    added photos table

--- models.py
+++ models.py
@@ -13,3 +13,17 @@ class Album(db.Models):
 #Сделать Photo - Арсений
 class Photo(db.Models):
     __tablename__='photos'
+
+    id = db.Column(db.Integer,primary_key=True)
+    user_id = db.Column(db.String(50), nullable = False, db.ForeignKey('albums.id'))
+    album_id = db.Column(db.String(50), nullable = False, db.ForeignKey('albums.id'))
+    path = db.Column(db.String(100), nullable = False)
+    comment = db.Column(db.String(50))
+    shot_date = db.Column(db.Date)
+
+    def __init__(self, user_id, album_id, path, comment, shot_date):
+        self.user_id = user_id
+        self.album_id = album_id
+        self.path = path
+        self.comment = comment
+        self.shot_date = shot_date
